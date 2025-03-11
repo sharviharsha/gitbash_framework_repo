ca374a6160bf13042e893e1dfbd6d048026c4c9a
sharviharsha <boomikam.rpee21@gmail.com>
2025-03-11 14:56:15 +0530

    org created

---
+++ src/test/java/org_Test/Create_Org.java
@@ -0,0 +1,13 @@
+package org_Test;
+
+import org.testng.annotations.Test;
+
+public class Create_Org 
+{
+	@Test
+	public void createOrganization()
+	{
+		System.out.println("createOrganization");
+	}
+
+}
